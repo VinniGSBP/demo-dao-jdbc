7ba15d7ecd261be0c01d81297190f02e708cd635
VinniGSBP <gs.vinni@gmail.com>
2025-11-22 00:43:12 -0300

    Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,12 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		System.out.println(obj);
+		
+		Seller sel = new Seller(1, "Vinicius", "csnois@gmail.com", new Date(), 3000.0, obj);
 
+		System.out.println();
+		
+		System.out.println(sel);
 	}
 
 }
