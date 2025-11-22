018e4a445b5fd7fef1e48a45380ab3e875eff3b0
VinniGSBP <gs.vinni@gmail.com>
2025-11-22 01:18:16 -0300

    findBy implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,21 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
 		
-		Seller sel = new Seller(1, "Vinicius", "csnois@gmail.com", new Date(), 3000.0, obj);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println();
+		
+		
+		Seller sel = sellerDao.findById(3);
+		
 		
 		System.out.println(sel);
 	}
